de1d18e836b958d67472596192393de8b905bc58
RKiddle <RKiddle@users.noreply.github.com>
2019-07-29 15:48:05 +0700

    Create Plotting_time_series_data.py

---
+++ Exploring_and_Visualizing_Customer_Behavior/Plotting_time_series_data.py
@@ -0,0 +1,15 @@
+# Group the data and aggregate first_week_purchases
+user_purchases = user_purchases.groupby(by=['reg_date', 'uid']).agg({'first_week_purchases': ['sum']})
+
+# Reset the indexes
+user_purchases.columns = user_purchases.columns.droplevel(level=1)
+user_purchases.reset_index(inplace=True)
+
+# Find the average number of purchases per day by first-week users
+user_purchases = user_purchases.groupby(by=['reg_date']).agg({'first_week_purchases': ['mean']})
+user_purchases.columns = user_purchases.columns.droplevel(level=1)
+user_purchases.reset_index(inplace=True)
+
+# Plot the results
+user_purchases.plot(x='reg_date', y='first_week_purchases')
+plt.show()
