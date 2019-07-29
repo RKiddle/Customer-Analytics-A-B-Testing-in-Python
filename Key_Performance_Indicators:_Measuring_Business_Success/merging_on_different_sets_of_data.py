078b89a56884e9f6ba96170cdf042bd9c605cd5e
RKiddle <RKiddle@users.noreply.github.com>
2019-07-29 14:55:58 +0700

    Create merging_on_different_sets_of_data.py

---
+++ Key_Performance_Indicators:_Measuring_Business_Success/merging_on_different_sets_of_data.py
@@ -0,0 +1,6 @@
+# Merge on the 'uid' and 'date' field
+uid_date_combined_data = app_purchases.merge(customer_data, on=['uid', 'date'], how='inner')
+
+# Examine the results 
+print(uid_date_combined_data.head())
+print(len(uid_date_combined_data))
