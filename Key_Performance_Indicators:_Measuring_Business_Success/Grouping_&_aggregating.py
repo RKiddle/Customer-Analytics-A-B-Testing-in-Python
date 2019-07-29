b7e18a7d9db696d37f8723793e428f6b2850c643
RKiddle <RKiddle@users.noreply.github.com>
2019-07-29 15:08:03 +0700

    Create Grouping_&_aggregating.py

---
+++ Key_Performance_Indicators:_Measuring_Business_Success/Grouping_&_aggregating.py
@@ -0,0 +1,8 @@
+# Group the data 
+grouped_purchase_data = purchase_data.groupby(by = ['device', 'gender'])
+
+# Aggregate the data
+purchase_summary = grouped_purchase_data.agg({'price': ['mean', 'median', 'std']})
+
+# Examine the results
+print(purchase_summary)
